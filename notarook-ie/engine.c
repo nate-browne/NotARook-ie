04d13e99c5f38b554ca153e7da8ca8c547c3ab1a
Nate Browne <npcompletenate@protonmail.com>
2021-02-10 14:54:56 -0800

    added temp loop to engine.c to test stuff

--- notarook-ie/engine.c
+++ notarook-ie/engine.c
@@ -7,12 +7,38 @@
 #include "constants.h"
 #include "functions.h"
 
+#define TEMPFEN "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N w - - 0 1"
 
 /**
  * The function that starts it all.
  */
-int main(int argc, char * argv[]) {
+int main(void) {
   init_all();
+
+  Board_t board;
+  char input[6];
+  int32_t move = NOMOVE;
+
+  parse_FEN(TEMPFEN, &board);
+
+  while(true) {
+    print_board(&board);
+
+    printf("Please enter a move: ");
+    fgets(input, sizeof(input), stdin);
+
+    if(*input == 'q') break;
+
+    if(*input == 't') {
+      take_move(&board);
+    } else {
+      move = parse_move(input, &board);
+      if(move) make_move(&board, move);
+    }
+
+    move = NOMOVE;
+    fflush(stdin);
+  }
 
   return EXIT_SUCCESS;
 }
