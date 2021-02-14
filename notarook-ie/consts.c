e49127aa6d6ff4f060c377061327f756e12a0ba8
Nate Browne <npcompletenate@protonmail.com>
2021-02-14 01:41:29 -0800

    cleaning up some definitions

--- notarook-ie/consts.c
+++ notarook-ie/consts.c
@@ -27,3 +27,14 @@ bool PIECE_ROOK_QUEEN[13] = {false, false, false, false, true, true, false, fals
 bool PIECE_BISHOP_QUEEN[13] = {false, false, false, true, false, true, false, false, false, true, false, true, false};
 bool PIECE_SLIDES[13] = {false, false, false, true, true, true, false, false, false, true, true, true, false};
 bool PIECE_KNIGHT[13] = {false, false, true, false, false, false, false, false, true, false, false, false, false};
+
+const int32_t MIRROR_64[STANDARD_BOARD_SIZE] = {
+56 , 57 , 58 , 59 , 60 , 61 , 62 , 63 ,
+48 , 49 , 50 , 51 , 52 , 53 , 54 , 55 ,
+40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 ,
+32 , 33 , 34 , 35 , 36 , 37 , 38 , 39 ,
+24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 ,
+16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 ,
+8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 ,
+0 , 1 , 2 , 3 , 4 , 5 , 6 , 7
+};
--- notarook-ie/evaluate.c
+++ notarook-ie/evaluate.c
@@ -49,19 +49,8 @@ static const int32_t ROOK_TABLE[STANDARD_BOARD_SIZE] = {
 0 , 0 , 5 , 10 , 10 , 5 , 0 , 0  
 };
 
-const int32_t MIRROR_64[STANDARD_BOARD_SIZE] = {
-56 , 57 , 58 , 59 , 60 , 61 , 62 , 63 ,
-48 , 49 , 50 , 51 , 52 , 53 , 54 , 55 ,
-40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 ,
-32 , 33 , 34 , 35 , 36 , 37 , 38 , 39 ,
-24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 ,
-16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 ,
-8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 ,
-0 , 1 , 2 , 3 , 4 , 5 , 6 , 7
-};
-
 // evaluation table for king in the end game
-const int32_t KING_E[STANDARD_BOARD_SIZE] = {
+static const int32_t KING_E[STANDARD_BOARD_SIZE] = {
  -50 , -20 , 0 , 0 , 0 , 0 , -20 , -50 ,
  -20, 0 , 20 , 20 , 20 , 20 , 0 , -20 ,
  0 , 20 , 40 , 40 , 40 , 40 , 20 , 0 ,
@@ -74,7 +63,7 @@ const int32_t KING_E[STANDARD_BOARD_SIZE] = {
 
 // evaluation table for kings in the opening
 // tries to encourage castling
-const int32_t KING_O[STANDARD_BOARD_SIZE] = {
+static const int32_t KING_O[STANDARD_BOARD_SIZE] = {
  0 , 5 , 5 , -10 , -10 , 0 , 10 , 5 ,
  -10 , -10 , -10 , -10 , -10 , -10 , -10 , -10 ,
  -30 , -30 , -30 , -30 , -30 , -30 , -30 , -30 ,
